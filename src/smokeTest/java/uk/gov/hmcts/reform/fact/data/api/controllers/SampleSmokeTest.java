abdeca66a5d91f72b1c862f9526bc87532bb76d6
Dan-Marius Bradea <bradeadanmarius@gmail.com>
2025-10-07 15:18:46 +0100

    added health and readiness checks

--- src/smokeTest/java/uk/gov/hmcts/reform/fact/data/api/controllers/SampleSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/fact/data/api/controllers/SampleSmokeTest.java
@@ -11,7 +11,7 @@ import static io.restassured.RestAssured.given;
 
 class SampleSmokeTest {
 
-    private String testUrl = System.getenv().getOrDefault("TEST_URL", "http://localhost:8989");
+    private final String testUrl = System.getenv().getOrDefault("TEST_URL", "http://localhost:8989");
 
     @BeforeEach
     void setUp() {
@@ -30,5 +30,34 @@ class SampleSmokeTest {
 
         Assertions.assertEquals(200, response.statusCode());
         Assertions.assertTrue(response.asString().startsWith("Welcome"));
+    }
+
+    @Test
+    void healthCheck() {
+        Response response = given()
+            .baseUri(testUrl)
+            .when()
+            .get("/health")
+            .then()
+            .statusCode(200)
+            .extract().response();
+
+        System.out.println("Health Status: " + response.jsonPath().getString("status"));
+        Assertions.assertEquals("UP", response.jsonPath().getString("status"));
+    }
+
+    @Test
+    void testReadinessCheck() {
+        Response response = given()
+            .baseUri(testUrl)
+            .when()
+            .get("/health/readiness")
+            .then()
+            .statusCode(200)
+            .extract().response();
+
+        System.out.println("DB Status: " + response.jsonPath().getString("status"));
+        System.out.println("DB Details: " + response.jsonPath().getString("components.db.status"));
+        Assertions.assertEquals("UP", response.jsonPath().getString("status"));
     }
 }
