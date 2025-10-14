ecd61be00bbb93bec92591ec6573fe5270cd146f
josh-uk <joshua.blackmoor@justice.gov.uk>
2025-10-14 14:54:02 +0100

    Added notNull check

--- src/main/java/uk/gov/hmcts/reform/fact/data/api/entities/CourtTranslation.java
+++ src/main/java/uk/gov/hmcts/reform/fact/data/api/entities/CourtTranslation.java
@@ -41,6 +41,7 @@ public class CourtTranslation {
     private UUID id;
 
     @Schema(description = "The ID of the associated Court", requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotNull
     @Column(name = "court_id")
     private UUID courtId;
 
