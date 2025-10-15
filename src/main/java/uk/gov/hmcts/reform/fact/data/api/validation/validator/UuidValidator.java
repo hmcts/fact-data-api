814afdb23af56c26e31320eb7521d5a937b63845
Josh <23316525+josh-uk@users.noreply.github.com>
2025-10-15 15:27:00 +0100

    FACT-2469 - Court Translation Services (#33)
    * WIP: Added endpoints but need to update with master
    
    * Functionality implemented
    
    * Update some validation things
    
    * Unit tests and checkstyle
    
    * Update int test
    
    * Allowed spaces in the phone number
    
    * Added int tests for new controller
    
    * Changes after entity merge
    
    * Set the court to stop hibernate warning
    
    * Added notNull check
    
    * Updated regex for email
    
    * Added message for 400 with uuid in body
    
    * Added unit test

---
+++ src/main/java/uk/gov/hmcts/reform/fact/data/api/validation/validator/UuidValidator.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.fact.data.api.validation.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import uk.gov.hmcts.reform.fact.data.api.validation.annotations.ValidUUID;
+
+import java.util.UUID;
+
+public class UuidValidator implements ConstraintValidator<ValidUUID, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null || value.isEmpty()) {
+            return false;
+        }
+
+        try {
+            UUID.fromString(value);
+            return true;
+        } catch (IllegalArgumentException ex) {
+            return false;
+        }
+    }
+}
+
