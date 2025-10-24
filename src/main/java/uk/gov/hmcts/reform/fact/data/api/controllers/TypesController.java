fc52a591df59776b7fd6e27c9d368656373b58fc
Rebecca <67150587+RebeccaHayleyPickles@users.noreply.github.com>
2025-10-24 13:04:39 +0100

    FACT-2480 Add types controller and service

---
+++ src/main/java/uk/gov/hmcts/reform/fact/data/api/controllers/TypesController.java
@@ -0,0 +1,113 @@
+package uk.gov.hmcts.reform.fact.data.api.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.fact.data.api.entities.AreaOfLawType;
+import uk.gov.hmcts.reform.fact.data.api.entities.ContactDescriptionType;
+import uk.gov.hmcts.reform.fact.data.api.entities.CourtType;
+import uk.gov.hmcts.reform.fact.data.api.entities.OpeningHourType;
+import uk.gov.hmcts.reform.fact.data.api.entities.Region;
+import uk.gov.hmcts.reform.fact.data.api.entities.ServiceArea;
+import uk.gov.hmcts.reform.fact.data.api.services.TypesService;
+
+import java.util.List;
+
+@Tag(
+    name = "Types",
+    description = "Operations related to types required for the functionality of both the admin portal and frontend")
+@RestController
+@Validated
+@RequestMapping("/types/")
+public class TypesController {
+
+    private final TypesService typesService;
+
+    public TypesController(TypesService typesService) {
+        this.typesService = typesService;
+    }
+
+    @GetMapping("/v1/areas-of-law")
+    @Operation(
+        summary = "Get all areas of law types",
+        description = "Fetch all areas of law types."
+            + "Returns empty list if no areas of law types exist."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retrieved areas of law types"),
+    })
+    public ResponseEntity<List<AreaOfLawType>> getAreasOfLawTypes() {
+        return ResponseEntity.ok(typesService.getAreaOfLawTypes());
+    }
+
+    @GetMapping("/v1/court-types")
+    @Operation(
+        summary = "Get all court types",
+        description = "Fetch all court types."
+            + "Returns empty list if no court types exist."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retrieved court types"),
+    })
+    public ResponseEntity<List<CourtType>> getCourtTypes() {
+        return ResponseEntity.ok(typesService.getCourtTypes());
+    }
+
+    @GetMapping("/v1/opening-hours-types")
+    @Operation(
+        summary = "Get all opening hours types",
+        description = "Fetch all opening hours types."
+            + "Returns empty list if no opening hours types exist."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retrieved opening hours types"),
+    })
+    public ResponseEntity<List<OpeningHourType>> getOpeningHoursTypes() {
+        return ResponseEntity.ok(typesService.getOpeningHoursTypes());
+    }
+
+    @GetMapping("/v1/contact-description-types")
+    @Operation(
+        summary = "Get all contact description types",
+        description = "Fetch all contact description types."
+            + "Returns empty list if no contact description types exist."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retrieved contact description types"),
+    })
+    public ResponseEntity<List<ContactDescriptionType>> getContactDescriptionTypes() {
+        return ResponseEntity.ok(typesService.getContactDescriptionTypes());
+    }
+
+    @GetMapping("/v1/regions")
+    @Operation(
+        summary = "Get all regions",
+        description = "Fetch all regions."
+            + "Returns empty list if no regions exist."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retrieved regions"),
+    })
+    public ResponseEntity<List<Region>> getRegions() {
+        return ResponseEntity.ok(typesService.getRegions());
+    }
+
+    @GetMapping("/v1/service-areas")
+    @Operation(
+        summary = "Get all service areas",
+        description = "Fetch all service areas."
+            + "Returns empty list if no service areas exist."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retrieved service areas"),
+    })
+    public ResponseEntity<List<ServiceArea>> getServiceAreas() {
+        return ResponseEntity.ok(typesService.getServiceAreas());
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/fact/data/api/services/TypesServiceTest.java
@@ -0,0 +1,208 @@
+package uk.gov.hmcts.reform.fact.data.api.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.fact.data.api.entities.AreaOfLawType;
+import uk.gov.hmcts.reform.fact.data.api.entities.ContactDescriptionType;
+import uk.gov.hmcts.reform.fact.data.api.entities.CourtType;
+import uk.gov.hmcts.reform.fact.data.api.entities.OpeningHourType;
+import uk.gov.hmcts.reform.fact.data.api.entities.Region;
+import uk.gov.hmcts.reform.fact.data.api.entities.ServiceArea;
+import uk.gov.hmcts.reform.fact.data.api.repositories.AreaOfLawTypeRepository;
+import uk.gov.hmcts.reform.fact.data.api.repositories.ContactDescriptionTypeRepository;
+import uk.gov.hmcts.reform.fact.data.api.repositories.CourtTypeRepository;
+import uk.gov.hmcts.reform.fact.data.api.repositories.OpeningHoursTypeRepository;
+import uk.gov.hmcts.reform.fact.data.api.repositories.RegionRepository;
+import uk.gov.hmcts.reform.fact.data.api.repositories.ServiceAreaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TypesServiceTest {
+
+    @Mock
+    private AreaOfLawTypeRepository areaOfLawTypeRepository;
+
+    @Mock
+    private CourtTypeRepository courtTypeRepository;
+
+    @Mock
+    private OpeningHoursTypeRepository openingHoursTypeRepository;
+
+    @Mock
+    private ContactDescriptionTypeRepository contactDescriptionTypeRepository;
+
+    @Mock
+    private RegionRepository regionRepository;
+
+    @Mock
+    private ServiceAreaRepository serviceAreaRepository;
+
+    @InjectMocks
+    private TypesService typesService;
+
+    private List<AreaOfLawType> areaOfLawTypes;
+    private List<CourtType> courtTypes;
+    private List<OpeningHourType> openingHourTypes;
+    private List<ContactDescriptionType> contactDescriptionTypes;
+    private List<Region> regions;
+    private List<ServiceArea> serviceAreas;
+
+    @BeforeEach
+    void setup() {
+        areaOfLawTypes = List.of(
+            AreaOfLawType.builder()
+                .id(UUID.randomUUID())
+                .name("area of law type")
+                .build());
+
+        courtTypes = List.of(
+            CourtType.builder()
+                .id(UUID.randomUUID())
+                .name("court type")
+                .build());
+
+        openingHourTypes = List.of(
+            OpeningHourType.builder()
+                .id(UUID.randomUUID())
+                .name("opening hour type")
+                .build());
+
+        contactDescriptionTypes = List.of(
+            ContactDescriptionType.builder()
+                .id(UUID.randomUUID())
+                .name("contact description")
+                .build());
+
+        regions = List.of(
+            Region.builder()
+                .id(UUID.randomUUID())
+                .name("region")
+                .build());
+
+        serviceAreas = List.of(
+            ServiceArea.builder()
+                .id(UUID.randomUUID())
+                .name("service area")
+                .build());
+    }
+
+    @Test
+    void getAreasOfLawTypesReturnsAreasOfLawTypesWhenFound() {
+        when(areaOfLawTypeRepository.findAll()).thenReturn(areaOfLawTypes);
+
+        List<AreaOfLawType> result = typesService.getAreaOfLawTypes();
+
+        assertThat(result).isEqualTo(areaOfLawTypes);
+    }
+
+    @Test
+    void getAreasOfLawTypesReturnsEmptyListWhenNoneFound() {
+        when(areaOfLawTypeRepository.findAll()).thenReturn(List.of());
+
+        List<AreaOfLawType> result = typesService.getAreaOfLawTypes();
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void getCourtTypesReturnsCourtTypesWhenFound() {
+        when(courtTypeRepository.findAll()).thenReturn(courtTypes);
+
+        List<CourtType> result = typesService.getCourtTypes();
+
+        assertThat(result).isEqualTo(courtTypes);
+    }
+
+    @Test
+    void getCourtTypesReturnsEmptyListWhenNoneFound() {
+        when(courtTypeRepository.findAll()).thenReturn(List.of());
+
+        List<CourtType> result = typesService.getCourtTypes();
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void getOpeningHoursTypesReturnsOpeningHoursTypesWhenFound() {
+        when(openingHoursTypeRepository.findAll()).thenReturn(openingHourTypes);
+
+        List<OpeningHourType> result = typesService.getOpeningHoursTypes();
+
+        assertThat(result).isEqualTo(openingHourTypes);
+    }
+
+    @Test
+    void getOpeningHoursTypesReturnsEmptyListWhenNoneFound() {
+        when(openingHoursTypeRepository.findAll()).thenReturn(List.of());
+
+        List<OpeningHourType> result = typesService.getOpeningHoursTypes();
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void getContactDescriptionTypesReturnsContactDescriptionTypesWhenFound() {
+        when(contactDescriptionTypeRepository.findAll()).thenReturn(contactDescriptionTypes);
+
+        List<ContactDescriptionType> result = typesService.getContactDescriptionTypes();
+
+        assertThat(result).isEqualTo(contactDescriptionTypes);
+    }
+
+    @Test
+    void getContactDescriptionTypesReturnsEmptyListWhenNoneFound() {
+        when(contactDescriptionTypeRepository.findAll()).thenReturn(List.of());
+
+        List<ContactDescriptionType> result = typesService.getContactDescriptionTypes();
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void getRegionsReturnsRegionsWhenFound() {
+        when(regionRepository.findAll()).thenReturn(regions);
+
+        List<Region> result = typesService.getRegions();
+
+        assertThat(result).isEqualTo(regions);
+    }
+
+    @Test
+    void getRegionsReturnsEmptyListWhenNoneFound() {
+        when(regionRepository.findAll()).thenReturn(List.of());
+
+        List<Region> result = typesService.getRegions();
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void getServiceAreasReturnsServiceAreasWhenFound() {
+        when(serviceAreaRepository.findAll()).thenReturn(serviceAreas);
+
+        List<ServiceArea> result = typesService.getServiceAreas();
+
+        assertThat(result).isEqualTo(serviceAreas);
+    }
+
+    @Test
+    void getServiceAreasReturnsEmptyListWhenNoneFound() {
+        when(serviceAreaRepository.findAll()).thenReturn(List.of());
+
+        List<ServiceArea> result = typesService.getServiceAreas();
+
+        assertThat(result).isEmpty();
+    }
+
+
+}
+
