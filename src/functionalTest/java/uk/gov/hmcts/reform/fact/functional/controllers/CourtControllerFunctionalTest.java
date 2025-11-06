4ee399766f704a92fa611faf08b250101b7f3036
josh-uk <joshua.blackmoor@justice.gov.uk>
2025-11-06 14:33:30 +0000

    Fix checkstyle

--- src/functionalTest/java/uk/gov/hmcts/reform/fact/functional/controllers/CourtControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/fact/functional/controllers/CourtControllerFunctionalTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.fact.data.api.entities.Court;
