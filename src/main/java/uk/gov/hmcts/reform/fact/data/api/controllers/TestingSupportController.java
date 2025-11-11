aae611672de32e77d7dd09039e831bf064a76e19
Josh <23316525+josh-uk@users.noreply.github.com>
2025-11-11 14:24:54 +0000

    FACT-2466 Court Controller (#45)
    * FACT-2466 work in progress on azure storage setup
    
    * WIP: Push up changes to test working in an env
    
    * Bumping chart version/ fixing aliases
    
    * Update int tests
    
    * Added missed config
    
    * Update suppressions
    
    * Temp coverage
    
    * Checkstyle
    
    * Add default for conn string so MI works
    
    * Fix pipeline deploys
    
    * WIP
    
    * Court photos done
    
    * Feature code all added
    
    * Added some unit tests
    
    * More unit tests
    
    * FACT-2466 fix sonar issue and add int tests
    
    * FACT-2466 clean up and added more tests
    
    * fixed docker compose to work with new vars
    
    * Properly check content type
    
    * 400 for -1
    
    * Testing DB connection in pipeline
    
    * added POST test
    
    * checkstyle change
    
    * Added example test and clean up config
    
    * Clean up further
    
    * Fix checkstyle
    
    * Added allure reports and Jenkins config to store it
    
    * change step
    
    * Steps need env
    
    * Update reportDir
    
    * Updated naming for allure reports
    
    * Clean name for smoke
    
    * Test storing history
    
    * Put back to working
    
    * functional tests for court controller
    
    * Added testing support API
    
    * added sugested edits and changed the types controller tests to reflect same coding style as CourtControllerFunctionalTests
    
    * Checkstyle corrections
    
    * added cleanup to court controller and clear assertions to types controller tests
    
    * checkstyle correction
    
    * Added CourtPhotoControllerFunctionalTests
    
    * added more photo controller test
    
    ---------
    
    Co-authored-by: hmcts-jenkins-d-to-i <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
    Co-authored-by: Dan-Marius Bradea <bradeadanmarius@gmail.com>

---
+++ src/main/java/uk/gov/hmcts/reform/fact/data/api/controllers/TestingSupportController.java
@@ -0,0 +1,49 @@
+package uk.gov.hmcts.reform.fact.data.api.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Size;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.fact.data.api.services.CourtService;
+
+@Tag(name = "Testing Support", description = "Utility endpoints for cleaning automated test data")
+@RestController
+@Validated
+@RequestMapping("/testing-support")
+@ConditionalOnProperty(prefix = "testingSupport", name = "enableApi", havingValue = "true")
+public class TestingSupportController {
+
+    private final CourtService courtService;
+
+    public TestingSupportController(CourtService courtService) {
+        this.courtService = courtService;
+    }
+
+    @DeleteMapping("/courts/name-prefix/{courtNamePrefix}")
+    @Operation(
+        summary = "Bulk delete courts by name prefix",
+        description = "Deletes all courts whose names start with the supplied prefix (case-insensitive)."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully deleted matching courts"),
+        @ApiResponse(responseCode = "400", description = "Invalid court name prefix supplied")
+    })
+    public ResponseEntity<String> deleteCourtsByNamePrefix(
+        @PathVariable
+        @Size(min = 1, max = 200, message = "Court name prefix must be between 1 and 200 characters")
+        String courtNamePrefix
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(courtService.deleteCourtsByNamePrefix(courtNamePrefix)
+                      + " court(s) with prefix " + courtNamePrefix + " deleted successfully");
+    }
+}
