f2aeff7b1560f0ce729f0732fb3a8417f4e57911
hurricanepilot <jonathan.coomber@justice.gov.uk>
2025-11-14 10:50:11 +0000

    Remove explicit count queries as they can be derived

--- src/main/java/uk/gov/hmcts/reform/fact/data/api/repositories/AuditRepository.java
+++ src/main/java/uk/gov/hmcts/reform/fact/data/api/repositories/AuditRepository.java
@@ -49,15 +49,6 @@ public interface AuditRepository extends JpaRepository<Audit, UUID> {
             User u on a.userId = u.id
         """;
 
-    String COUNT_WITH_USER_JOIN = """
-        select
-            count(*)
-        from
-            Audit a
-        join
-            User u on a.userId = u.id
-        """;
-
     String WHERE_EMAIL_LIKE_AND_CREATED_AT_AFTER = """
         where
             a.createdAt > :createdAtAfter
@@ -70,34 +61,30 @@ public interface AuditRepository extends JpaRepository<Audit, UUID> {
             and u.email like %:email%
         """;
 
-    String WHERE_COURT_ID_EQUALS = " and a.courtId = :courtId";
+    String AND_COURT_ID_EQUALS = " and a.courtId = :courtId";
 
 
-    @Query(value = SELECT_WITH_USER_JOIN + WHERE_EMAIL_LIKE_AND_CREATED_AT_AFTER,
-        countQuery = COUNT_WITH_USER_JOIN + WHERE_EMAIL_LIKE_AND_CREATED_AT_AFTER)
+    @Query(value = SELECT_WITH_USER_JOIN + WHERE_EMAIL_LIKE_AND_CREATED_AT_AFTER)
     Page<Audit> findByCreatedAtAfterAndEmailAddressLike(
         ZonedDateTime createdAtAfter,
         String email,
         Pageable pageable);
 
-    @Query(value = SELECT_WITH_USER_JOIN + WHERE_EMAIL_LIKE_AND_CREATED_AT_BETWEEN,
-        countQuery = COUNT_WITH_USER_JOIN + WHERE_EMAIL_LIKE_AND_CREATED_AT_BETWEEN)
+    @Query(value = SELECT_WITH_USER_JOIN + WHERE_EMAIL_LIKE_AND_CREATED_AT_BETWEEN)
     Page<Audit> findByCreatedAtBetweenAndEmailAddressLike(
         ZonedDateTime createdAtAfter,
         ZonedDateTime createdAtBefore,
         String email,
         Pageable pageable);
 
-    @Query(value = SELECT_WITH_USER_JOIN + WHERE_EMAIL_LIKE_AND_CREATED_AT_AFTER + WHERE_COURT_ID_EQUALS,
-        countQuery = COUNT_WITH_USER_JOIN + WHERE_EMAIL_LIKE_AND_CREATED_AT_AFTER + WHERE_COURT_ID_EQUALS)
+    @Query(value = SELECT_WITH_USER_JOIN + WHERE_EMAIL_LIKE_AND_CREATED_AT_AFTER + AND_COURT_ID_EQUALS)
     Page<Audit> findByCourtIdAndCreatedAtAfterAndEmailAddressLike(
         UUID courtId,
         ZonedDateTime createdAtAfter,
         String email,
         Pageable pageable);
 
-    @Query(value = SELECT_WITH_USER_JOIN + WHERE_EMAIL_LIKE_AND_CREATED_AT_BETWEEN + WHERE_COURT_ID_EQUALS,
-        countQuery = COUNT_WITH_USER_JOIN + WHERE_EMAIL_LIKE_AND_CREATED_AT_BETWEEN + WHERE_COURT_ID_EQUALS)
+    @Query(value = SELECT_WITH_USER_JOIN + WHERE_EMAIL_LIKE_AND_CREATED_AT_BETWEEN + AND_COURT_ID_EQUALS)
     Page<Audit> findByCourtIdAndCreatedAtBetweenAndEmailAddressLike(
         UUID courtId,
         ZonedDateTime createdAtAfter,
