f40b97c53023f5c20376a33a72b1103c394837e8
amstevenson <addstevenson@hotmail.com>
2026-01-12 11:58:52 +0000

    add script in docker compose instead for extensions

--- docker/postgres/init/001-extensions.sql
+++ docker/postgres/init/001-extensions.sql
@@ -1,2 +1,3 @@
+-- Extensions locally required for the geo calculations between two points
 CREATE EXTENSION IF NOT EXISTS cube WITH SCHEMA public;
 CREATE EXTENSION IF NOT EXISTS earthdistance WITH SCHEMA public;
