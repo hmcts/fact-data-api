c6816a1c4fcc8afc88f434f9af3515337e33389a
amstevenson <addstevenson@hotmail.com>
2026-02-04 11:48:07 +0000

    adding slug GET endpoints

--- src/main/java/uk/gov/hmcts/reform/fact/data/api/repositories/CourtDetailsRepository.java
+++ src/main/java/uk/gov/hmcts/reform/fact/data/api/repositories/CourtDetailsRepository.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.fact.data.api.repositories;
 
 import uk.gov.hmcts.reform.fact.data.api.entities.CourtDetails;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourtDetailsRepository extends JpaRepository<CourtDetails, UUID> {
+    /**
+     * Finds a court details record by slug.
+     *
+     * @param slug the court slug
+     * @return matching court details if present
+     */
+    Optional<CourtDetails> findBySlug(String slug);
 }
