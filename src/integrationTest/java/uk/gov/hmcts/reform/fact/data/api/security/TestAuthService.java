77243bec1784b74a523c168ff937c9d0946fa3a7
hurricanepilot <jonathan.coomber@justice.gov.uk>
2026-02-24 08:06:34 +0000

    Added test authService to allow preAuthorize calls.
    And tidied up the AuditControllerTest

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/fact/data/api/security/TestAuthService.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.fact.data.api.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+/**
+ * Allows full {@link org.springframework.boot.test.context.SpringBootTest} integration tests to function.
+ */
+@Service("authService")
+@Primary
+@Slf4j
+public class TestAuthService {
+    public boolean canView() {
+        return true;
+    }
+
+    public boolean isAdmin() {
+        return true;
+    }
+}
