a1362ccbe84a4deb8f29ab7cee8830a70e68ff80
Josh <23316525+josh-uk@users.noreply.github.com>
2026-03-03 15:51:06 +0000

    FACT-2547 photo migration (#72)
    * initial commit on migration
    
    * latest changes when removing postcodes etc
    
    * removing autowired as no longer needed
    
    * reverting postcode change
    
    * removing not needed stuff
    
    * moving exceptions for migrating data
    
    * removing postcode stuff
    
    * integration test passing
    
    * removing not needed autowireds
    
    * webmvc page
    
    * adding in missing mig, dx code changes and so forth
    
    * regex updates and such
    
    * renaming migrations
    
    * migrations to add service centre region plus mig audit
    
    * removing data
    
    * mig changes, plus service changes
    
    * reducing size of service
    
    * Added migration route for photos
    
    * Change to long
    
    * Unit tests
    
    * fixing int tests
    
    * removing unused import
    
    * adding unit tests
    
    * checkstyle fix
    
    * adding another test
    
    * adding more responses, removing unneeded build gradle lines
    
    * moving away from records
    
    * removing unneeded docstring
    
    * removing top level docstrings
    
    * revering to what it was before
    
    * feign client added instead of rest template
    
    * pr changes
    
    * adding test context for feign client int tests and restoring method
    
    * resolving dependency
    
    * updating to allow for aat test data
    
    * adding env var for envs
    
    * Bumping chart version/ fixing aliases
    
    * Fix issue
    
    * Master fix
    
    * Fix flyway script
    
    * reset script to master
    
    * Fixed broken tests
    
    * FACT-2486 (#138)
    
    * merge conflicts, local autority court map by name to uuid id
    
    * updates for quality
    
    * adding docstring
    
    * int test fix
    
    * bump flyway script
    
    * Created a list
    
    * Fix issue
    
    * bump flyway
    
    * Setup auth for migration controller
    
    * Remove these tests
    
    * new way for auth at controller level
    
    * ensure resource closing for photo url input stream
    
    ---------
    
    Co-authored-by: amstevenson <addstevenson@hotmail.com>
    Co-authored-by: hmcts-jenkins-d-to-i <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: hurricanepilot <hurricanepilot@gmail.com>

--- src/main/java/uk/gov/hmcts/reform/fact/data/api/migration/controller/MigrationController.java
+++ src/main/java/uk/gov/hmcts/reform/fact/data/api/migration/controller/MigrationController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import uk.gov.hmcts.reform.fact.data.api.migration.model.MigrationResponse;
 import uk.gov.hmcts.reform.fact.data.api.migration.model.MigrationSummary;
+import uk.gov.hmcts.reform.fact.data.api.migration.model.PhotoMigrationResponse;
 import uk.gov.hmcts.reform.fact.data.api.migration.service.MigrationService;
+import uk.gov.hmcts.reform.fact.data.api.migration.service.PhotoMigrationService;
 import uk.gov.hmcts.reform.fact.data.api.security.SecuredFactRestController;
 
 @SecuredFactRestController(
@@ -22,6 +24,7 @@ import uk.gov.hmcts.reform.fact.data.api.security.SecuredFactRestController;
 public class MigrationController {
 
     private final MigrationService migrationService;
+    private final PhotoMigrationService photoMigrationService;
 
     @PostMapping("/import")
     @Operation(
@@ -41,5 +44,15 @@ public class MigrationController {
             "Migration completed successfully",
             summary.getResult()
         ));
+    }
+
+    @PostMapping("/photos")
+    @Operation(
+        summary = "Migrate court photos from legacy FaCT system",
+        description = "Fetches court photos from the legacy FaCT private migration endpoint "
+            + "and persists them into the new database and storage account."
+    )
+    public ResponseEntity<PhotoMigrationResponse> importCourtPhotos() {
+        return ResponseEntity.ok(photoMigrationService.migratePhotos());
     }
 }
