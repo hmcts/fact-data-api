93db1a20ede6dc838da8b444436421594132072d
hurricanepilot <hurricanepilot@gmail.com>
2026-03-12 16:28:25 +0000

    switched to a get mapping for court generation

--- src/main/java/uk/gov/hmcts/reform/fact/data/api/controllers/TestingSupportController.java
+++ src/main/java/uk/gov/hmcts/reform/fact/data/api/controllers/TestingSupportController.java
@@ -15,8 +15,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,7 +53,7 @@ public class TestingSupportController {
                       + " court(s) with prefix " + courtNamePrefix + " deleted successfully");
     }
 
-    @PostMapping("/courts")
+    @GetMapping("/courts")
     @Operation(
         summary = "Create sample court",
         description = "Creates a sample court with randomised data for testing purposes."
