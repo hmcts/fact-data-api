580e8e1f976c977677e0063d36bbfe75dfd9016f
josh-uk <joshua.blackmoor@justice.gov.uk>
2026-03-19 15:31:44 +0000

    Clean up todos

--- src/main/java/uk/gov/hmcts/reform/fact/data/api/entities/types/Page.java
+++ src/main/java/uk/gov/hmcts/reform/fact/data/api/entities/types/Page.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.fact.data.api.entities.types;
 
-//TODO: FACT-2479 - placeholder enum
 public enum Page {
     COURT,
     COURT_ACCESSIBILITY,
--- src/main/java/uk/gov/hmcts/reform/fact/data/api/entities/types/ServiceAreaType.java
+++ src/main/java/uk/gov/hmcts/reform/fact/data/api/entities/types/ServiceAreaType.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.fact.data.api.entities.types;
 
-// TODO: placeholder enum
 public enum ServiceAreaType {
     CIVIL,
     FAMILY,
