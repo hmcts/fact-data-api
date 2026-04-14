c21af51b5d9cd6430d43f5947b9e7dcf88d67118
Rebecca <67150587+RebeccaHayleyPickles@users.noreply.github.com>
2026-04-14 12:23:12 +0100

    Create flyway migration to remove extra apostrophes

---
+++ src/main/resources/db/migration/V1.21__fix_service_area_description_double_apostrophe.sql
@@ -0,0 +1,3 @@
+UPDATE public.service_area SET description = 'Applying for probate, storing a will, settlement and disputes over a deceased person''s property, money and possessions.' WHERE name = 'Probate';
+UPDATE public.service_area SET description_cy = 'Apelio yn erbyn penderfyniad am anghydfod cyflogaeth sy''n ymwneud â thâl, diswyddo annheg a gwahaniaethu.' WHERE name = 'Claims against employers';
+
