e0d6cdd5513a7094c574294547e6337b6141bbc9
amstevenson <addstevenson@hotmail.com>
2026-06-25 15:36:16 +0100

    new value

--- src/main/resources/db/migration/V1.37__add_bailiff_office_opening_hour_type.sql
+++ src/main/resources/db/migration/V1.37__add_bailiff_office_opening_hour_type.sql
@@ -1,3 +1,3 @@
 -- Add Bailiff office opening hour type
 INSERT INTO public.opening_hour_types (id, name, name_cy)
-VALUES (gen_random_uuid(), 'Bailiff office open', 'Oriau agor swyddfa''r Beiliaid');
+VALUES (gen_random_uuid(), 'Bailiff office open', 'Mae swyddfa''r beilïaid ar agor');
