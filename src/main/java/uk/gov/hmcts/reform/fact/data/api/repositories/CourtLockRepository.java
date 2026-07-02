380a8805202d6979d57c13a971a11aa7cb0cf8fa
hurricanepilot <hurricanepilot@gmail.com>
2026-07-02 12:32:52 +0100

    Add full User entity to the JSON payload

--- src/main/java/uk/gov/hmcts/reform/fact/data/api/repositories/CourtLockRepository.java
+++ src/main/java/uk/gov/hmcts/reform/fact/data/api/repositories/CourtLockRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.fact.data.api.entities.types.Page;
@@ -14,8 +15,10 @@ import uk.gov.hmcts.reform.fact.data.api.entities.types.Page;
 public interface CourtLockRepository extends JpaRepository<CourtLock, UUID> {
     void deleteAllByUserId(UUID userId);
 
-    List<CourtLock> findAllByCourtId(UUID userId);
+    @EntityGraph(attributePaths = {"user"})
+    List<CourtLock> findAllByCourtId(UUID courtId);
 
+    @EntityGraph(attributePaths = {"user"})
     Optional<CourtLock> findByCourtIdAndPage(UUID courtId, Page page);
 
     void deleteByCourtIdAndPage(UUID courtId, Page page);
