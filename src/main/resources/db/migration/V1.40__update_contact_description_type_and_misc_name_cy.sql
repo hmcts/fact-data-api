5e9bef585ac5e90be4a4d46cd7185dc490a04e4a
debolina-seal <debolina.seal@gmail.com>
2026-07-08 17:02:26 +0100

    Addressing RC

--- src/main/resources/db/migration/V1.40__update_contact_description_type_and_misc_name_cy.sql
+++ src/main/resources/db/migration/V1.40__update_contact_description_type_and_misc_name_cy.sql
@@ -134,3 +134,59 @@ FROM (
         ('Transcripts', 'Trawsgrifiadau')
 ) AS v(name, name_cy)
 WHERE cdt.name = v.name;
+
+ALTER TABLE court_types
+  ADD COLUMN IF NOT EXISTS name_cy VARCHAR;
+
+UPDATE court_types ct
+SET name_cy = v.name_cy
+  FROM (
+    VALUES
+        ('Magistrates'' Court', 'Llys Ynadon'),
+        ('Family Court', 'Llys Teulu'),
+        ('Tribunal', 'Tribiwnlys'),
+        ('County Court', 'Llys Sirol'),
+        ('Crown Court', 'Llys y Goron')
+) AS v(name, name_cy)
+WHERE ct.name = v.name;
+
+UPDATE area_of_law_types aolt
+SET name_cy = v.name_cy
+  FROM (
+    VALUES
+        ('Divorce', 'Ysgariad'),
+        ('Civil partnership', 'Partneriaeth sifil'),
+        ('High Court District Registry', 'Cofrestrfa Ddosbarth yr Uchel Lys'),
+        ('Adoption', 'Mabwysiadu'),
+        ('Social security', 'Nawdd Cymdeithasol'),
+        ('Housing possession', 'Meddiannu eiddo'),
+        ('Money claims', 'Hawliadau am arian'),
+        ('Employment', 'Cyflogaeth'),
+        ('Tax', 'Treth'),
+        ('Children', 'Plant'),
+        ('Domestic violence', 'Trais domestig'),
+        ('Crime', 'Trosedd'),
+        ('Civil', 'Sifil'),
+        ('Single justice procedure', 'Y Weithdrefn Un Ynad'),
+        ('High Court', 'Yr Uchel Lys'),
+        ('Court Of Appeal', 'Y Llys Apêl'),
+        ('Financial Remedy', 'Rhwymedi Ariannol'),
+        ('Business and Property', 'Busnes ac Eiddo'),
+        ('Bankruptcy', 'Methdaliad'),
+        ('Immigration', 'Mewnfudo'),
+        ('Probate', 'Profiant'),
+        ('Forced marriage', 'Priodas dan orfod'),
+        ('FGM', 'Anffurfio Organau Cenhedlu Benywod (FGM)'),
+        ('Pathfinder – for Private Law Proceedings', 'Cynllun Braenaru – ar gyfer Achosion Cyfraith Breifat'),
+        ('Domestic Abuse Protection Order (DAPOs)', 'Gorchymyn Diogelu Rhag Cam-drin Domestig (DAPOs)'),
+        ('Benefits', 'Budd-daliadau')
+) AS v(name, name_cy)
+WHERE aolt.name = v.name;
+
+ALTER TABLE court_dxcodes
+  ADD COLUMN IF NOT EXISTS explanation_cy VARCHAR(250);
+
+ALTER TABLE court_fax
+  ADD COLUMN IF NOT EXISTS description_cy VARCHAR(250);
+
+
--- src/main/resources/db/migration/V1.43__add_welsh_columns_for_dx_and_fax.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE court_dxcodes
-    ADD COLUMN IF NOT EXISTS explanation_cy VARCHAR(250);
-
-ALTER TABLE court_fax
-    ADD COLUMN IF NOT EXISTS description_cy VARCHAR(250);
-
