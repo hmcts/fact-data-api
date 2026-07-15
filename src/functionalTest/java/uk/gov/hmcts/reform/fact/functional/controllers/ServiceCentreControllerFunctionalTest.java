ed40d3ae42fd6fdb2a79252d07a7918f54ba12e0
josh-uk <joshua.blackmoor@justice.gov.uk>
2026-07-15 14:14:51 +0100

    Added warning notice for welsh

--- src/functionalTest/java/uk/gov/hmcts/reform/fact/functional/controllers/ServiceCentreControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/fact/functional/controllers/ServiceCentreControllerFunctionalTest.java
@@ -54,12 +54,14 @@ public final class ServiceCentreControllerFunctionalTest {
         serviceCentre.setName(TestDataHelper.appendRandomSuffixToCourtName(TEST_PREFIX + " Updated"));
         serviceCentre.setOpen(true);
         serviceCentre.setWarningNotice("Updated warning notice");
+        serviceCentre.setWarningNoticeCy("Rhybudd wedi'i ddiweddaru");
         serviceCentre.setCatchmentType(CatchmentType.NATIONAL);
 
         Response updateResponse = http.doPut("/service-centres/" + serviceCentreId + "/v1", serviceCentre);
         AssertionHelper.assertStatus(updateResponse, OK);
         assertThat(updateResponse.jsonPath().getBoolean("open")).isTrue();
         assertThat(updateResponse.jsonPath().getString("warningNotice")).isEqualTo("Updated warning notice");
+        assertThat(updateResponse.jsonPath().getString("warningNoticeCy")).isEqualTo("Rhybudd wedi'i ddiweddaru");
         assertThat(updateResponse.jsonPath().getString("catchmentType")).isEqualTo(CatchmentType.NATIONAL.name());
 
         Response missingResponse = http.doGet("/service-centres/" + UUID.randomUUID() + "/v1");
