8624b18f06ff2d3bcc8d783d67ca0c9a3c07d4c1
Ibrahim Elsanosi <ibrahim.elsanosi@justice.gov.uk>
2026-07-23 17:21:56 +0100

    FACT-2886 make lock unique constraint migration idempotent

--- src/main/resources/db/migration/V1.47__add_unique_constraint_to_lock.sql
+++ src/main/resources/db/migration/V1.47__add_unique_constraint_to_lock.sql
@@ -1,3 +1,19 @@
 -- Guarantee only one lock can ever exist for subject_id/subject_type page combination
-ALTER TABLE lock
-  ADD CONSTRAINT unique_lock_subject_page UNIQUE (subject_id, subject_type, page);
+DO $$
+DECLARE
+    lock_table regclass := to_regclass(format('%I.%I', current_schema(), 'lock'));
+BEGIN
+    IF lock_table IS NOT NULL
+        AND NOT EXISTS (
+            SELECT 1
+            FROM pg_constraint
+            WHERE conname = 'unique_lock_subject_page'
+              AND conrelid = lock_table
+        ) THEN
+        EXECUTE format(
+            'ALTER TABLE %s ADD CONSTRAINT unique_lock_subject_page UNIQUE (subject_id, subject_type, page)',
+            lock_table
+        );
+    END IF;
+END
+$$;
