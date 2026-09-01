8f7e01a82bf4722248301e6a91caa8657b6cdcdd
ibrahimelsanosi-moj <ibrahim.elsanosi@justice.gov.uk>
2026-09-01 17:02:00 +0100

    FACT-2890 update table area of law types

--- src/main/resources/db/migration/V1.54__update_area_of_law_types.sql
+++
@@ -1,9 +0,0 @@
--- Rename the Area of Law types for Welsh.
-UPDATE area_of_law_types aolt
-SET display_name_cy = v.display_name_cy
-  FROM (
-    VALUES
-        ('Court Of Appeal', 'Y Llys Apêl'),
-        ('High Court', 'Yr Uchel Lys')
-) AS v(name, display_name_cy)
-WHERE aolt.name = v.name;
