905660dae0b7cb68795d19655b6e76f938d0d903
amstevenson <addstevenson@hotmail.com>
2026-09-03 16:57:12 +0100

    func and int test updates

--- src/functionalTest/java/uk/gov/hmcts/reform/fact/functional/controllers/search/SearchAddressControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/fact/functional/controllers/search/SearchAddressControllerFunctionalTest.java
@@ -4,9 +4,13 @@ import io.qameta.allure.Feature;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.fact.data.api.os.OsData;
+import uk.gov.hmcts.reform.fact.data.api.os.OsLpi;
 import uk.gov.hmcts.reform.fact.data.api.os.OsResult;
 import uk.gov.hmcts.reform.fact.functional.helpers.TestDataHelper;
 import uk.gov.hmcts.reform.fact.functional.http.HttpClient;
+
+import java.util.List;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,9 +20,10 @@ public final class SearchAddressControllerFunctionalTest {
 
     private static final HttpClient http = new HttpClient();
     private static final String STABLE_ENGLAND_POSTCODE = "SW1A 1AA";
+    private static final String STABLE_LPI_POSTCODE = "DH1 3RG";
 
     @Test
-    @DisplayName("GET /search/address/v1/postcode/{postcode} returns addresses for valid postcode")
+    @DisplayName("GET /search/address/v1/postcode/{postcode} returns valid DPA or LPI address results")
     void shouldReturnAddressesForValidPostcode() throws Exception {
         final OsData osData = TestDataHelper.fetchOsDataForPostcode(http, STABLE_ENGLAND_POSTCODE);
 
@@ -26,15 +31,47 @@ public final class SearchAddressControllerFunctionalTest {
             .as("Expected non-empty results for postcode %s", STABLE_ENGLAND_POSTCODE)
             .isNotNull()
             .isNotEmpty()
-            .extracting(OsResult::getDpa)
-            .as("Each result should contain valid DPA (Delivery Point Address) data")
-            .allSatisfy(dpa -> assertThat(dpa.getPostcode())
-                .as("DPA POSTCODE should contain outward code SW1A")
-                .contains("SW1A"))
-            .as("At least one result should have a non-blank address and postcode")
-            .anySatisfy(dpa -> {
-                assertThat(dpa.getAddress()).as("DPA ADDRESS should be non-blank").isNotBlank();
-                assertThat(dpa.getPostcode()).as("DPA POSTCODE should be non-blank").isNotBlank();
+            .allSatisfy(result -> {
+                assertThat(result.getDpa() != null || result.getLpi() != null)
+                    .as("Each result should contain DPA or LPI address data")
+                    .isTrue();
+
+                if (result.getDpa() != null) {
+                    assertThat(result.getDpa().getAddress()).as("DPA ADDRESS should be non-blank").isNotBlank();
+                    assertThat(result.getDpa().getPostcode())
+                        .as("DPA POSTCODE should contain outward code SW1A")
+                        .contains("SW1A");
+                } else {
+                    assertThat(result.getLpi().getAddress()).as("LPI ADDRESS should be non-blank").isNotBlank();
+                    assertThat(result.getLpi().getPostcodeLocator())
+                        .as("LPI POSTCODE_LOCATOR should contain outward code SW1A")
+                        .contains("SW1A");
+                }
+            });
+
+        assertThat(osData.getResults())
+            .as("Expected at least one DPA result for postcode %s", STABLE_ENGLAND_POSTCODE)
+            .anySatisfy(result -> assertThat(result.getDpa()).isNotNull());
+    }
+
+    @Test
+    @DisplayName("GET /search/address/v1/postcode/{postcode} supports an LPI address postcode")
+    void shouldReturnLpiAddressesForKnownLpiPostcode() throws Exception {
+        final OsData osData = TestDataHelper.fetchOsDataForPostcode(http, STABLE_LPI_POSTCODE);
+        final List<OsLpi> lpiResults = osData.getResults().stream()
+            .map(OsResult::getLpi)
+            .filter(Objects::nonNull)
+            .toList();
+
+        assertThat(lpiResults)
+            .as("Expected LPI results for postcode %s", STABLE_LPI_POSTCODE)
+            .isNotEmpty()
+            .allSatisfy(lpi -> {
+                assertThat(lpi.getAddress()).as("LPI ADDRESS should be non-blank").isNotBlank();
+                assertThat(lpi.getPostcodeLocator())
+                    .as("LPI POSTCODE_LOCATOR should match the requested postcode")
+                    .isEqualTo(STABLE_LPI_POSTCODE);
+                assertThat(lpi.getUprn()).as("LPI UPRN should be non-blank").isNotBlank();
             });
     }
 }
