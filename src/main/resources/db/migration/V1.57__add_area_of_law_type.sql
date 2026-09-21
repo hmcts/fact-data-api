f0cf45e28350551be704fd3c6d732e7c0a5b30dc
ibrahimelsanosi-moj <ibrahim.elsanosi@justice.gov.uk>
2026-09-21 10:45:32 +0100

    FACT-3061 Add Court of protection under cases heard

--- src/main/resources/db/migration/V1.57__add_area_of_law_type.sql
+++ src/main/resources/db/migration/V1.57__add_area_of_law_type.sql
@@ -1,4 +1,4 @@
--- Add Court of protection under cases heard.
+-- Add the "Court of protection" area of law type for the Cases Heard category.
 INSERT INTO public.area_of_law_types (id, name, name_cy)
 values(gen_random_uuid(), 'Court of protection', 'Y Llys Gwarchod');
 
