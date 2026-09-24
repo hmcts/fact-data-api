8bca9dd48ed515248329c0c136303fa8cac5ed3a
hurricanepilot <hurricanepilot@gmail.com>
2026-09-24 14:03:04 +0100

    Add workaround for preview only being accessible from AAT

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -28,7 +28,7 @@ provider "azurerm" {
 provider "azurerm" {
   features {}
   alias           = "aks-preview"
-  subscription_id = "8b6ea922-0862-443e-af15-6056e1c9b9a4"
+  subscription_id = var.aks_preview_subscription_id
 }
 
 provider "azurerm" {
