c7e7d4e8d5d07d3a42b81e560f1b78e1d46b6e88
Jon <hurricanepilot@gmail.com>
2026-09-25 11:14:42 +0100

    FACT-2942 - privatise access to the blob stores (PR2) (#422)
    * Add jenkins as valid subnets for sa access
    
    * switch back to "container" for container access level
    
    * change updates to only be configuration of the sa_subnets
    
    * add aks-infra provider
    
    * add aks_preview_subscription_id to vars
    
    * remove aks_preview_subscription_id from vars
    
    * Add workaround for preview only being accessible from AAT
    
    * add conditional resolution of the preview subnets based on env

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -18,6 +18,15 @@ variable "aks_subscription_id" {
   default = ""
 }
 
+variable "mgmt_subscription_id" {
+  default = ""
+}
+
+variable "aks_preview_subscription_id" {
+  default = "8b6ea922-0862-443e-af15-6056e1c9b9a4"
+  description = "Preview subscription id for AAT; set to the same as aks_subscription_id for other environments"
+}
+
 variable "jenkins_AAD_objectId" {
   default = ""
 }
